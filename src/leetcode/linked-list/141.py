1a709158147b78aff8840cf2effb9d6cdc3834db
Jeremy Mullen <jeremymullen@stockx.com>
2024-05-18 09:32:01 -0400

    Create 141.py

---
+++ src/leetcode/linked-list/141.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast:
+            fast = fast.next
+            if fast:
+                slow = slow.next
+                fast = fast.next
+
+            if slow == fast:
+                return True
+        return False
