058871700eb6e0e95cb532738b770599d0795ea0
Jeremy <jeremymullen@stockx.com>
2024-07-02 10:42:24 -0400

    Solve problem 659

--- src/leetcode/array/659.py
+++ src/leetcode/array/659.py
@@ -1,32 +1,35 @@
-class Solution:
-    """
-    @param: strs: a list of strings
-    @return: encodes a list of strings to a single string.
-    """
-    def encode(self, strs):
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
         encoded_str = ''
         for s in strs:
             # number of characters followed by special character followed by the word, e.g. '4|word'
             encoded_str += str(len(s)) + '|' + s
         return encoded_str
 
-    """
-    @param: str: A string
-    @return: decodes a single string to a list of strings
-    """
-    def decode(self, str):
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
         decoded = []
         i = 0
 
-        while i < len(str):
+        while i < len(s):
             j = i
-            while j < len(str) and str[j] != '|':
+            while s[j] != '|':
                 j += 1
             # i will be the start of our size block
             # j will be the location of our special character "|"
-            size = int(str[i:j])  # Elements i->j will contain the length characters
+            size = int(s[i:j])  # Elements i->j will contain the length characters
             # Characters after j contain the word, with size telling us how many characters the word is
-            word = str[j + 1: j + 1 + size]
+            i = j + 1 # bump `i` to the start of the actual word
+            j = i + size # Bump `j` to the end of the actual word
+            word = s[i:j] # Parse the actual word
             decoded.append(word)
-            i += j + 1 + size  # Move the pointer forward to the start of the next size block
+            i = j  # Move the pointer forward to the start of the next size block
         return decoded
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
