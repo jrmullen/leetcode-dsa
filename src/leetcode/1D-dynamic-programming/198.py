a51b8a65e67ceab7ffc17f5a9abe0f036f21b979
Jeremy Mullen <jeremyrmullen@gmail.com>
2024-08-18 15:16:47 -0400

    Add comments

--- src/leetcode/1D-dynamic-programming/198.py
+++ src/leetcode/1D-dynamic-programming/198.py
@@ -3,8 +3,14 @@ class Solution:
         n1 = 0
         n2 = 0
 
+        # Visit each house in `nums`, updating each element in place with the maximum value possible
         for n in nums:
+            # Adjacent houses cannot be robbed, a comparison must be made between the adjacent house `n2`
+            # and the new house `n`, which would be added to the running sum of loot from 2 houses ago, `n1`
+            # Visualized - n1 n2 n
             sum = max(n1 + n, n2)
+
+            # Move the pointers forward
             n1 = n2
             n2 = sum
 
