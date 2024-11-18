2ab85b60e28f696e6ff3365d2e2c021c0e3f849d
Jeremy Mullen <jeremyrmullen@gmail.com>
2024-11-18 08:48:46 -0500

    Optimize problem 424 solution

--- src/leetcode/sliding-window/424.py
+++ src/leetcode/sliding-window/424.py
@@ -1,17 +1,28 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        result = 0
         l = 0
-        longest = 0
-        frequencies = {}
+        count = defaultdict(int)
+        maxFreq = 0
 
+        # Intuition: Maintain a sliding window. K values in the window may be replaced.
+        # Therefore there may be up to K characters in the window that do not match the most frequent character.
+        # So a valid window may contain up to K characters differing from the most frequent character.
+        # windowLength = K + maxFreq
         for r in range(len(s)):
-            frequencies[s[r]] = 1 + frequencies.get(s[r], 0)
+            count[s[r]] += 1
 
-            # If the window size - count of most frequent characters > k
-            # it is not a valid string, so we must shift the left point until it is valid again
-            while (r - l + 1) - max(frequencies.values()) > k:
-                frequencies[s[l]] -= 1
+            maxFreq = max(maxFreq, count[s[r]])
+
+            
+
+            # If the window size - maxFreq exceeds size K, the window is no longer valid, so the left pointer must
+            # be shifted to shrink the window until it is once again valid.
+            # Remember windowLength = K + maxFreq, which means windowLength - maxFreq <= K
+            while (r - l + 1) - maxFreq > k:
+                count[s[l]] -= 1
                 l += 1
             
-            longest = max(longest, r - l + 1)
-        return longest
+            result = max(result, r - l + 1)
+            
+        return result
