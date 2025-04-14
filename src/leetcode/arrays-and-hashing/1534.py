f48b21f27976e92be22b792a75449f986c3013c1
Jeremy Mullen <jeremyrmullen@gmail.com>
2025-04-14 19:53:11 -0400

    Solve problem 1534

---
+++ src/leetcode/arrays-and-hashing/1534.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        n = len(arr)
+        result = 0
+
+        # O(n^3) Iterate over each possible tripleteeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                for k in range(j + 1, n):
+                    # If the constraints are met, increment the `result` counter of valid triplets
+                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        result += 1
+        
+        return result
