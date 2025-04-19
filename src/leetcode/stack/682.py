f88e1f3fbd0394f5d16a4f77fa7be811a9289ee9
Jeremy Mullen <jeremyrmullen@gmail.com>
2025-04-18 23:37:48 -0400

    Solve problem 682

--- src/leetcode/stack/682.py
+++ src/leetcode/stack/682.py
@@ -1,14 +1,16 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
-        stack = []
+        stack = [] # Initialize a stack to store scores
 
         for operation in operations:
             if operation == 'C': # Invalidate the previous score
                 stack.pop()
-            elif operation == 'D':
+            elif operation == 'D': # Record a new score that is the double of the previous score
                 stack.append(stack[-1] * 2)
             elif operation == '+': # Record a new score that is the sum of the previous two scores
                 stack.append(stack[-2] + stack[-1])
-            else:
+            else: # Record a new score of `operation`
                 stack.append(int(operation))
+        
+        # Return the sum of all elements on the stack
         return sum(stack)
